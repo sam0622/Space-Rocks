deac63d4cb71182cde312d5166ed44b5252f7088
sam0622 <sgreenfield517@gmail.com>
2023-10-31 08:40:47 -0400

    rock go boom

--- assets/Main/main.gd
+++ assets/Main/main.gd
@@ -9,7 +9,7 @@ func _ready():
 	for i in 3:
 		spawn_rock(3)
 		
-func spawn_rock(size, pos=null, vel=null): # Spawns rocks
+func spawn_rock(size, pos=null, vel=null):
 	if pos == null:
 		$RockPath/RockSpawn.progress = randi()
 		pos = $RockPath/RockSpawn.position
@@ -19,5 +19,17 @@ func spawn_rock(size, pos=null, vel=null): # Spawns rocks
 	r.screensize = screensize
 	r.start(pos, vel, size)
 	call_deferred("add_child", r)
-
+	r.exploded.connect(self._on_rock_exploded)
+	
+	
+	
+func _on_rock_exploded(size, radius, pos, vel):
+	if size <= 1:
+		return
+	for offset in [-1, 1]:
+		var dir = $Player.position.direction_to(pos).orthogonal() * offset
+		var newpos = pos + dir * radius
+		var newvel = dir * vel.length() * 1.1
+		spawn_rock(size - 1, newpos, newvel)
+		
 
--- assets/Rock/Rock.gd
+++ assets/Rock/Rock.gd
@@ -1,10 +1,30 @@
 extends RigidBody2D
+
 signal exploded
 
 var screensize = Vector2.ZERO
 var size
 var radius
 var scale_factor = 0.2
+
+func start(_position, _velocity, _size):
+	position = _position
+	size = _size
+	mass = 1.5 * size
+	$Sprite2D.scale = Vector2.ONE * scale_factor * size
+	radius = int($Sprite2D.texture.get_size().x / 2 * scale_factor * size)
+	var shape = CircleShape2D.new()
+	shape.radius = radius
+	$CollisionShape2D.shape = shape
+	linear_velocity = _velocity
+	angular_velocity = randf_range(-PI, PI)
+	$Explosion.scale = Vector2.ONE * 0.75 * size
+	
+func _integrate_forces(physics_state):
+	var xform = physics_state.transform
+	xform.origin.x = wrapf(xform.origin.x, 0 - radius, screensize.x + radius)
+	xform.origin.y = wrapf(xform.origin.y, 0 - radius, screensize.y + radius)
+	physics_state.transform = xform
 
 func explode():
 	$CollisionShape2D.set_deferred("disabled", true)
@@ -16,25 +36,3 @@ func explode():
 	angular_velocity = 0
 	await $Explosion/AnimationPlayer.animation_finished
 	queue_free()
-
-
-
-func start(_position, _velocity, _size): #Calculates hitbox size based on rock size
-	position = _position
-	size = _size
-	mass = 1.5 * size
-	$Sprite2D.scale = Vector2.ONE * scale_factor * size
-	radius = int($Sprite2D.texture.get_size().x / 2 * 
-		$Sprite2D.scale.x)
-	var shape = CircleShape2D.new()
-	shape.radius = radius
-	$CollisionShape2D.shape = shape
-	linear_velocity = _velocity
-	angular_velocity = randf_range(-PI,PI)
-	$Explosion.scale = Vector2.ONE * 0.75 * size
-	
-func _integrate_forces(physics_state):
-	var xform = physics_state.transform
-	xform.origin.x = wrapf(xform.origin.x, 0 - radius, screensize.x + radius)
-	xform.origin.y = wrapf(xform.origin.y, 0 - radius, screensize.y + radius)
-	physics_state.transform = xform
